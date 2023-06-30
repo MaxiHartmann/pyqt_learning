dc4ad3033b7b9e0a832c13f1a652154dfcf19b3b
MaxiHartmann <maxi.hartmann207@gmail.com>
2023-06-30 21:43:27 +0200

    init cool tabwidget ...

---
+++ tabwidget/create_data.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+x = np.linspace(0, 1, 30)
+
+### Probe 1
+y = np.sin(2 * np.pi * x) 
+df = pd.DataFrame({"x": x, "y": y})
+noise = np.random.normal(1, 0.1, 30)
+df_noise = pd.DataFrame({"x": x, "y": y * noise})
+df.to_csv("./data_1/probe1.csv", index=False)
+df.to_csv("./data_2/probe1.csv", index=False)
+
+### Probe 2
+y = np.sin(2 * np.pi * x) + 2
+df = pd.DataFrame({"x": x, "y": y})
+df_noise = pd.DataFrame({"x": x, "y": y * noise})
+
+df.to_csv("./data_1/probe2.csv", index=False)
+df_noise.to_csv("./data_2/probe2.csv", index=False)
+
+### Probe 3
+y = 3 * np.sin(2 * np.pi * x) + 2
+df = pd.DataFrame({"x": x, "y": y})
+df_noise = pd.DataFrame({"x": x, "y": y * noise})
+
+df.to_csv("./data_1/probe3.csv", index=False)
+df_noise.to_csv("./data_2/probe3.csv", index=False)
---
+++ tabwidget/setup.py
@@ -0,0 +1,47 @@
+import glob
+import pandas as pd
+
+class Setup:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+
+        self.search_for_probes()
+        self.load_probe(0)
+
+    def search_for_probes(self):
+
+        path = self.path
+
+        files = glob.glob(f"{path}**/*probe*.csv", recursive=True)
+        files.sort()
+        files.append("None")
+
+        self.probefiles = files
+        self.selected_probe = 0
+
+    def load_probe(self, index):
+        filename = self.probefiles[self.selected_probe]
+        if filename != "None":
+            df = pd.read_csv(filename)
+            self.data = df
+        else:
+            self.data = None
+            
+    def print_setup(self):
+        # maybe better in json.dumps?!
+        print(f"name: {self.name}")
+        print(f"path: {self.path}")
+        print(f"data: {self.data}")
+        # setup["Data"] = df.to_dict()
+
+
+if __name__ == '__main__':
+
+    setups = []
+    setups.append(Setup("Setup_A", "./data_1/"))
+    setups.append(Setup("Setup_B", "./data_2/"))
+    setups.append(Setup("Setup_C", "./data_2/"))
+
+    for s in setups:
+        s.print_setup()
